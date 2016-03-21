56704cc07bc71d4e724e5674f1ca29bd0784b47f
telemanagerplus <jaiclaicliat@emeil.ir>
2016-03-21 17:43:05 +0430

    Create calc.lua

---
+++ plugins/calc.lua
@@ -0,0 +1,68 @@
+-- Function reference: http://mathjs.org/docs/reference/functions/categorical.html
+
+local function mathjs(exp)
+  local url = 'http://api.mathjs.org/v1/'
+  url = url..'?expr='..URL.escape(exp)
+  local b,c = http.request(url)
+  local text = nil
+  if c == 200 then
+    text = '= '..b
+  
+  elseif c == 400 then
+    text = b
+  else
+    text = 'Unexpected error\n'
+      ..'Is api.mathjs.org up?'
+  end
+  return text
+end
+
+local function run(msg, matches)
+  return mathjs(matches[1])
+end
+
+return {
+  description = "Calculate math expressions with mathjs API",
+  usage = "!calc [expression]: evaluates the expression and sends the result.",
+  patterns = {
+    "^!calc (.*)$",
+	"^[Cc]alc (.*)$"
+
+  },
+  run = run
+}
+
+
+
+
+--  -_-_-_-_-_-_-_-_-_-   ||-_-_-_-_-_   ||             ||-_-_-_-_-_
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||-_-_-_-_-_   ||             ||-_-_-_-_-_
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||-_-_-_-_-_   ||-_-_-_-_-_   ||-_-_-_-_-_
+--
+--
+--                               /\                              /\             /-_-_-_-_-_    ||-_-_-_-_-_   ||-_-_-_-_-_
+--  ||\\            //||        //\\        ||      //||        //\\           //              ||             ||         //
+--  || \\          // ||       //  \\       ||     // ||       //  \\         //               ||             ||       //
+--  ||  \\        //  ||      //    \\      ||    //  ||      //    \\       ||                ||             ||    //
+--  ||   \\      //   ||     //______\\     ||   //   ||     //______\\      ||      -_-_-_-   ||-_-_-_-_-_   || //
+--  ||    \\    //    ||    //        \\    ||  //    ||    //        \\     ||           ||   ||             ||  \\ 
+--  ||     \\  //     ||   //          \\   || //     ||   //          \\     \\          ||   ||             ||     \\
+--  ||      \\//      ||  //            \\  ||//      ||  //            \\     \\-_-_-_-_-||   ||-_-_-_-_-_   ||        \\
+--
+--
+--  ||-_-_-_-    ||           ||           ||               //-_-_-_-_-_-
+--  ||     ||    ||           ||           ||              //
+--  ||_-_-_||    ||           ||           ||             //
+--  ||           ||           ||           ||             \\
+--  ||           ||           \\           //              \\
+--  ||           ||            \\         //               //
+--  ||           ||-_-_-_-_     \\-_-_-_-//    -_-_-_-_-_-//
+--
+--By @ali_ghoghnoos
+--@telemanager_ch
