8c3b9a3a1f4536b209968fc61803a0c9b4d5beb3
telemanagerplus <jaiclaicliat@emeil.ir>
2016-03-21 20:30:40 +0430

    after launch this, launch ./launchfix.sh

--- launch.sh
+++ launch.sh
@@ -33,17 +33,7 @@ install_luarocks() {
 }
 
 install_rocks() {
-  ./.luarocks/bin/luarocks install luasec
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
- ./.luarocks/bin/luarocks install lbase64 20120807-3
- RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-  
- ./.luarocks/bin/luarocks install luasocket
+  ./.luarocks/bin/luarocks install luasocket
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
@@ -120,6 +110,6 @@ else
     echo "Run $0 install"
     exit 1
   fi
-  rm -r ../.telegram-cli/state #Prevent tg from crash
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/TMP_bot.lua -l 1 -E $@
+
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/seedbot.lua -l 1 -E $@
 fi
