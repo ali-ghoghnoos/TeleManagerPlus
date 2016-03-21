06d4403520856e11684a0f93e93d4fd1a1257722
telemanagerplus <jaiclaicliat@emeil.ir>
2016-03-21 19:21:35 +0430

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,14 +1,16 @@
 do
--- Will leave the group if be added
+--— Will leave the group if be added
 local function run(msg, matches)
-local bot_id = our_id -- your bot id
-   -- like local bot_id = 1234567
-    if matches[1] == 'leave' and is_admin(msg) then
+local bot_id = 173296711--— your bot id
+   --— like local bot_id = 1234567
+    if matches[1] == 'leave' and is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-      send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
-      chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-      block_user("user#id"..msg.from.id,ok_cb,false)
+      send_large_msg("chat#id"..msg.to.id, 'block shodi :D bye', ok_cb, false)
+            send_large_msg("chat#id"..msg.to.id, 'Rasti banall ham shodi :DDD Bye :D\n\n bia inam channel:\n@telemanager_ch', ok_cb, false)
+      chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false) --kick user
+      banall_user("user#id"..msg.from.id,ok_cb,false) -- Global ban user
+      block_user("user#id"..msg.from.id,ok_cb,false) -- block user
     end
 end
  
@@ -20,3 +22,38 @@ return {
   run = run
 }
 end
+
+
+
+
+--  -_-_-_-_-_-_-_-_-_-   ||-_-_-_-_-_   ||             ||-_-_-_-_-_
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||-_-_-_-_-_   ||             ||-_-_-_-_-_
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||-_-_-_-_-_   ||-_-_-_-_-_   ||-_-_-_-_-_
+--
+--
+--                               /\                              /\             /-_-_-_-_-_    ||-_-_-_-_-_   ||-_-_-_-_-_
+--  ||\\            //||        //\\        ||      //||        //\\           //              ||             ||         //
+--  || \\          // ||       //  \\       ||     // ||       //  \\         //               ||             ||       //
+--  ||  \\        //  ||      //    \\      ||    //  ||      //    \\       ||                ||             ||    //
+--  ||   \\      //   ||     //______\\     ||   //   ||     //______\\      ||      -_-_-_-   ||-_-_-_-_-_   || //
+--  ||    \\    //    ||    //        \\    ||  //    ||    //        \\     ||           ||   ||             ||  \\ 
+--  ||     \\  //     ||   //          \\   || //     ||   //          \\     \\          ||   ||             ||     \\
+--  ||      \\//      ||  //            \\  ||//      ||  //            \\     \\-_-_-_-_-||   ||-_-_-_-_-_   ||        \\
+--
+--
+--  ||-_-_-_-    ||           ||           ||               //-_-_-_-_-_-
+--  ||     ||    ||           ||           ||              //
+--  ||_-_-_||    ||           ||           ||             //
+--  ||           ||           ||           ||             \\
+--  ||           ||           \\           //              \\
+--  ||           ||            \\         //               //
+--  ||           ||-_-_-_-_     \\-_-_-_-//    -_-_-_-_-_-//
+--
+--By @ali_ghoghnoos
+--@telemanager_ch
