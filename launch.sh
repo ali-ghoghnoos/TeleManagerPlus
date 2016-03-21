96b1eed540d75ba06dfb974e6b7ddecd23860c7a
telemanagerplus <jaiclaicliat@emeil.ir>
2016-03-21 20:20:10 +0430

    fix launching problem when you want run new bot ! xD

--- launch.sh
+++ launch.sh
@@ -33,7 +33,17 @@ install_luarocks() {
 }
 
 install_rocks() {
-  ./.luarocks/bin/luarocks install luasocket
+  ./.luarocks/bin/luarocks install luasec
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+ ./.luarocks/bin/luarocks install lbase64 20120807-3
+ RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+  
+ ./.luarocks/bin/luarocks install luasocket
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
@@ -110,6 +120,6 @@ else
     echo "Run $0 install"
     exit 1
   fi
-
+  rm -r ../.telegram-cli/state #Prevent tg from crash
   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/TMP_bot.lua -l 1 -E $@
 fi
