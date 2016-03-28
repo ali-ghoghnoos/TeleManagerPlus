d4af30f013ebe57f8064b663e13fb7b9e8c3f2b3
telemanagerplus <jaiclaicliat@emeil.ir>
2016-03-28 16:43:06 +0430

    Create set_type.lua

---
+++ plugins/set_type.lua
@@ -0,0 +1,59 @@
+local function run(msg, matches, callback, extra)
+
+local data = load_data(_config.moderation.data)
+
+local group_type = data[tostring(msg.to.id)]['group_type']
+
+if matches[1] and is_sudo(msg) then
+    
+data[tostring(msg.to.id)]['group_type'] = matches[1]
+        save_data(_config.moderation.data, data)
+        
+        return 'Group Type Seted To : '..matches[1]
+
+end
+if not is_sudo(msg) then 
+    return 'You Are Not Allow To set Group Type !'
+    end
+end
+return {
+  patterns = {
+  "^[!/]type (.*)$",
+  },
+  run = run
+}
+
+
+
+
+--  -_-_-_-_-_-_-_-_-_-   ||-_-_-_-_-_   ||             ||-_-_-_-_-_
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||-_-_-_-_-_   ||             ||-_-_-_-_-_
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||-_-_-_-_-_   ||-_-_-_-_-_   ||-_-_-_-_-_
+--
+--
+--                               /\                              /\             /-_-_-_-_-_    ||-_-_-_-_-_   ||-_-_-_-_-_
+--  ||\\            //||        //\\        ||      //||        //\\           //              ||             ||         //
+--  || \\          // ||       //  \\       ||     // ||       //  \\         //               ||             ||       //
+--  ||  \\        //  ||      //    \\      ||    //  ||      //    \\       ||                ||             ||    //
+--  ||   \\      //   ||     //______\\     ||   //   ||     //______\\      ||      -_-_-_-   ||-_-_-_-_-_   || //
+--  ||    \\    //    ||    //        \\    ||  //    ||    //        \\     ||           ||   ||             ||  \\ 
+--  ||     \\  //     ||   //          \\   || //     ||   //          \\     \\          ||   ||             ||     \\
+--  ||      \\//      ||  //            \\  ||//      ||  //            \\     \\-_-_-_-_-||   ||-_-_-_-_-_   ||        \\
+--
+--
+--  ||-_-_-_-    ||           ||           ||               //-_-_-_-_-_-
+--  ||     ||    ||           ||           ||              //
+--  ||_-_-_||    ||           ||           ||             //
+--  ||           ||           ||           ||             \\
+--  ||           ||           \\           //              \\
+--  ||           ||            \\         //               //
+--  ||           ||-_-_-_-_     \\-_-_-_-//    -_-_-_-_-_-//
+--
+--By @ali_ghoghnoos
+--@telemanager_ch
