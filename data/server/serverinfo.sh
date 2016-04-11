588bfec20c8d06b93f4aa31e3c78999a9f9ee62a
telemanagerplus <jaiclaicliat@emeil.ir>
2016-04-11 10:05:45 +0430

    Dont Change anything ! (only 1 bug needed to fu*k Your server !

---
+++ data/server/serverinfo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+memTotal_b=`free -b |grep Mem |awk '{print $2}'`
+memFree_b=`free -b |grep Mem |awk '{print $4}'`
+memBuffer_b=`free -b |grep Mem |awk '{print $6}'`
+memCache_b=`free -b |grep Mem |awk '{print $7}'`
+
+memTotal_m=`free -m |grep Mem |awk '{print $2}'`
+memFree_m=`free -m |grep Mem |awk '{print $4}'`
+memBuffer_m=`free -m |grep Mem |awk '{print $6}'`
+memCache_m=`free -m |grep Mem |awk '{print $7}'`
+CPUPer=`top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`
+hdd=`df -lh | awk '{if ($6 == "/") { print $5 }}' | head -1 | cut -d'%' -f1`
+uptime=`uptime`
+ProcessCnt=`ps -A | wc -l`
+memUsed_b=$(($memTotal_b-$memFree_b-$memBuffer_b-$memCache_b))
+memUsed_m=$(($memTotal_m-$memFree_m-$memBuffer_m-$memCache_m))
+
+memUsedPrc=$((($memUsed_b*100)/$memTotal_b))
+
+echo "Memory: CRITICAL Total: $memTotal_m MB"
+echo "Memory Used: $memUsed_m MB - $memUsedPrc% used!"
+echo "---------------"
+echo 'CPU Usage : '"$CPUPer"'%'
+echo "---------------"
+echo 'Hdd : '"$hdd"'%'
+echo "---------------"
+echo 'Processes : '"$ProcessCnt"
+echo "---------------"
+echo 'Uptime : '"$uptime"
+echo "---------------"
+echo "By : @TeleManager_ch"
