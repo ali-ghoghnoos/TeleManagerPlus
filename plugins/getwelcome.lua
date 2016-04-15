c130bdacc7005c513a6dfd2b95dc10bfd95f27ce
telemanagerplus <jaiclaicliat@emeil.ir>
2016-04-15 14:26:00 +0430

    create getwelcome plugin ! (only work with setwelcome.lua)

---
+++ plugins/getwelcome.lua
@@ -0,0 +1,58 @@
+
+--[[Only Worked With setwelcome.lua plugin ! ]]--
+
+local function run(msg, matches, callback, extra)
+
+local data = load_data(_config.moderation.data)
+
+local group_welcome = data[tostring(msg.to.id)]['group_welcome']
+
+if matches[1] == 'chat_add_user' or 'chat_add_user_link' then --For Normal Group
+    if not msg.service then 
+        return nil 
+    else
+        return group_welcome
+end
+end
+end
+return {
+  patterns = {
+  "^!!tgservice (chat_add_user)$",
+  "^!!tgservice (chat_add_user_link)$",
+  },
+  run = run,
+  pre_process = pre_process
+}
+
+
+--  -_-_-_-_-_-_-_-_-_-   ||-_-_-_-_-_   ||             ||-_-_-_-_-_
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||-_-_-_-_-_   ||             ||-_-_-_-_-_
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||             ||             ||
+--           ||           ||-_-_-_-_-_   ||-_-_-_-_-_   ||-_-_-_-_-_
+--
+--
+--                               /\                              /\             /-_-_-_-_-_    ||-_-_-_-_-_   ||-_-_-_-_-_
+--  ||\\            //||        //\\        ||      //||        //\\           //              ||             ||         //
+--  || \\          // ||       //  \\       ||     // ||       //  \\         //               ||             ||       //
+--  ||  \\        //  ||      //    \\      ||    //  ||      //    \\       ||                ||             ||    //
+--  ||   \\      //   ||     //______\\     ||   //   ||     //______\\      ||      -_-_-_-   ||-_-_-_-_-_   || //
+--  ||    \\    //    ||    //        \\    ||  //    ||    //        \\     ||           ||   ||             ||  \\ 
+--  ||     \\  //     ||   //          \\   || //     ||   //          \\     \\          ||   ||             ||     \\
+--  ||      \\//      ||  //            \\  ||//      ||  //            \\     \\-_-_-_-_-||   ||-_-_-_-_-_   ||        \\
+--
+--
+--  ||-_-_-_-    ||           ||           ||               //-_-_-_-_-_-
+--  ||     ||    ||           ||           ||              //
+--  ||_-_-_||    ||           ||           ||             //
+--  ||           ||           ||           ||             \\
+--  ||           ||           \\           //              \\
+--  ||           ||            \\         //               //
+--  ||           ||-_-_-_-_     \\-_-_-_-//    -_-_-_-_-_-//
+--
+-- By @ali_ghoghnoos
+-- @telemanager_ch
